c1b6f9e925dd1ac93904db1a4ffec4e9da24d436
Christopher Kwong <kwongochris@gmail.com>
2022-05-17 02:30:20 -0700

    added run args

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ def main():
     from frontend import create_webapp
     create_webapp(app)
 
-    app.run(host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=8080)
 
 
 if __name__ == "__main__":
