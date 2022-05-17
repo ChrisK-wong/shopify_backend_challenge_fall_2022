83609be16093df69dd66ec430938490de1f7cf55
Christopher Kwong <kwongochris@gmail.com>
2022-05-17 14:47:01 -0700

    cleaned code and added comments

--- api/inventory.py
+++ api/inventory.py
@@ -8,10 +8,18 @@ items = Blueprint('api/items', __name__, url_prefix='/api/items')
 @items.route('/', methods=['GET'])
 @items.route('/<int:id>', methods=['GET'])
 def read(id=''):
+    """
+    Get item(s) from database
+
+    :param id: int
+    :return: json
+    """
     if id:
+        # Try to get item from database
         item = Inventory.query.get_or_404(id)
         return jsonify(view_item(item))
     else:
+        # Get all items from database
         items = Inventory.query.all()
         result = []
         for item in items:
@@ -21,17 +29,22 @@ def read(id=''):
 
 @items.route('/', methods=['POST'])
 def create():
+    """
+    Add new item to database
+
+    Requires json {"name": str, "quantity": int}
+    :return: json
+    """
+    # Get keys/values from json
     data = request.get_json()
     item_name = data.get('name')
+    item_quantity = data.get('quantity')
     if not item_name:
         abort(400)
-    try:
-        item_quantity = int(data.get('quantity'))
-        if item_quantity < 0:
-            abort(400)
-    except (ValueError, TypeError):
+    if not item_quantity.isdigit() or int(item_quantity) < 0:
         abort(400)
-    item = Inventory(name=item_name, quantity=item_quantity)
+    # Create item in database
+    item = Inventory(name=item_name, quantity=int(item_quantity))
     db.session.add(item)
     db.session.commit()
     return jsonify(success=True)
@@ -39,16 +52,22 @@ def create():
 
 @items.route('/<int:id>/update', methods=['PUT'])
 def update(id):
+    """
+    Update data of item in database
+
+    Requires json {"name": str, "quantity": int}
+    :param id: int
+    :return: json
+    """
+    # Get keys/values from json
     data = request.get_json()
     item_name = data.get('name')
+    item_quantity = data.get('quantity')
     if not item_name:
         abort(400)
-    try:
-        item_quantity = int(data.get('quantity'))
-        if item_quantity < 0:
-            abort(400)
-    except (ValueError, TypeError):
+    if not item_quantity.isdigit() or int(item_quantity) < 0:
         abort(400)
+    # Try to get item from database and update it
     item = Inventory.query.get_or_404(id)
     item.name = item_name
     item.quantity = item_quantity
@@ -58,6 +77,13 @@ def update(id):
 
 @items.route('/<int:id>/delete', methods=['DELETE'])
 def delete(id):
+    """
+    Delete item from database
+
+    :param id: int
+    :return: json
+    """
+    # Try to get item from database and delete it
     item = Inventory.query.get_or_404(id)
     db.session.delete(item)
     db.session.commit()
--- api/shipment.py
+++ api/shipment.py
@@ -8,11 +8,19 @@ shipments = Blueprint('api/shipments', __name__, url_prefix='/api/shipments')
 @shipments.route('/', methods=['GET'])
 @shipments.route('/<int:id>', methods=['GET'])
 def read(id=''):
+    """
+    Get shipment(s) from database
+
+    :param id: int
+    :return: json
+    """
     if id:
+        # Try to get shipment from database
         shipment = Shipments.query.get_or_404(id)
         return jsonify(view_shipment(shipment))
 
     else:
+        # Get all shipments from database
         shipments = Shipments.query.all()
         result = []
         for s in shipments:
@@ -22,11 +30,19 @@ def read(id=''):
 
 @shipments.route('/', methods=['POST'])
 def create():
+    """
+    Add new shipment to database
+
+    Requires json {"description": str, "address": str}
+    :return: json
+    """
+    # Get keys/values from json
     data = request.get_json()
     ship_desc = data.get('description')
     ship_addr = data.get('address')
     if not (ship_desc and ship_addr):
         abort(400)
+    # Create shipment in database
     ship_details = Shipments(description=ship_desc, address=ship_addr)
     db.session.add(ship_details)
     db.session.commit()
@@ -35,11 +51,20 @@ def create():
 
 @shipments.route('/<int:id>/update', methods=['PUT'])
 def update(id):
+    """
+    Update data of shipment in database
+
+    Requires json {"description": str, "address": str}
+    :param id: int
+    :return: json
+    """
+    # Get keys/values from json
     data = request.get_json()
     ship_desc = data.get('description')
     ship_addr = data.get('address')
     if not (ship_desc and ship_addr):
         abort(400)
+    # Try to get shipment from database and update it
     shipment = Shipments.query.get_or_404(id)
     shipment.description = ship_desc
     shipment.address = ship_addr
@@ -49,7 +74,15 @@ def update(id):
 
 @shipments.route('/<int:id>/delete', methods=['DELETE'])
 def delete(id):
+    """
+    Delete shipment from database and adjust inventory accordingly
+
+    :param id: int
+    :return: json
+    """
+    # Try to get shipment from database and delete it
     shipment = Shipments.query.get_or_404(id)
+    # Add back items to inventory
     for ship_items in shipment.items:
         item = Inventory.query.get(ship_items.item_id)
         item.quantity += ship_items.quantity
@@ -60,24 +93,35 @@ def delete(id):
 
 @shipments.route('/<int:ship_id>/add', methods=['POST'])
 def add(ship_id):
+    """
+    Add an item to a shipment and adjust inventory accordingly
+
+    Requires json {"item_id": int, "quantity": int}
+    :param ship_id: int
+    :return: json
+    """
+    # get keys/values from json
     data = request.get_json()
-    try:
-        item_id = int(data.get('item_id'))
-        item_quantity = int(data.get('quantity'))
-        if item_quantity < 1:
-            abort(400)
-    except (ValueError, TypeError):
+    item_id = data.get('item_id')
+    item_quantity = data.get('quantity')
+    if not item_id.isdigit():
         abort(400)
+    if not item_quantity.isdigit() or int(item_quantity) < 1:
+        abort(400)
+    # Try to get shipment and item from database
     shipment = Shipments.query.get_or_404(ship_id)
     item = Inventory.query.get_or_404(item_id)
-    if item.quantity < item_quantity:
+    # Check if enough of item in inventory
+    if item.quantity < int(item_quantity):
         abort(409)
-    item.quantity -= item_quantity
+    item.quantity -= int(item_quantity)
     ship_item = ShipmentItems.query.filter_by(ship_id=ship_id, item_id=item_id).first()
     if ship_item:
-        ship_item.quantity += item_quantity
+        # Item already in shipment
+        ship_item.quantity += int(item_quantity)
     else:
-        ship_item = ShipmentItems(quantity=item_quantity)
+        # Add new item to shipment
+        ship_item = ShipmentItems(quantity=int(item_quantity))
         ship_item.item = item
         shipment.items.append(ship_item)
     db.session.commit()
@@ -86,8 +130,17 @@ def add(ship_id):
 
 @shipments.route('/<int:ship_id>/<int:item_id>/remove', methods=['DELETE'])
 def remove(ship_id, item_id):
+    """
+    Remove an item from a shipment and adjust inventory accordingly
+
+    :param ship_id: int
+    :param item_id: int
+    :return: json
+    """
+    # Try to get shipment from database
     ship_item = ShipmentItems.query.filter_by(ship_id=ship_id, item_id=item_id).first()
     if ship_item:
+        # Try to item from database and increment quantity from shipment
         item = Inventory.query.get_or_404(item_id)
         item.quantity += ship_item.quantity
         db.session.delete(ship_item)
