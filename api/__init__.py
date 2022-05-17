79fce0280f29955ca40e91c7cdf1f087c3a8823b
Christopher Kwong <kwongochris@gmail.com>
2022-05-17 02:24:39 -0700

    added comments

--- api/__init__.py
+++ api/__init__.py
@@ -3,12 +3,20 @@ from .models import *
 
 
 def create_app():
+    """
+    Build flask app and initiate handlers and blueprints
+
+    :return: flask app object
+    """
+
     app = __create_app()
     __error_handlers(app)
 
+    # route: /items
     from .inventory import items
     app.register_blueprint(items)
 
+    # route: /shipments
     from .shipment import shipments
     app.register_blueprint(shipments)
 
@@ -16,12 +24,22 @@ def create_app():
 
 
 def create_database():
+    """
+    Create database tables with flask app. This only needs to be ran once.
+
+    :return: None
+    """
     app = __create_app()
     with app.app_context():
         db.create_all()
 
 
 def __create_app():
+    """
+    Helper function to create flask app with database
+
+    :return: flask app object
+    """
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
     db.init_app(app)
@@ -29,6 +47,12 @@ def __create_app():
 
 
 def __error_handlers(app):
+    """
+    Create error handlers for flask app
+
+    :param app: flask app object
+    :return: None
+    """
     @app.errorhandler(400)
     def handle_400_error(error):
         return jsonify({'error': 'Invalid request'}), 400
--- run.py
+++ run.py
@@ -1,7 +1,20 @@
-from api import create_app
-app = create_app()
+def main():
+    """
+    Runs app with backend and frontend
 
-from frontend import create_webapp
-create_webapp(app)
+    :return: None
+    """
 
-app.run()
+    # Create API
+    from api import create_app
+    app = create_app()
+
+    # Create frontend
+    from frontend import create_webapp
+    create_webapp(app)
+
+    app.run(host="0.0.0.0")
+
+
+if __name__ == "__main__":
+    main()
